d15cd9b6baa607d4c0071f68cc246dc60f6c3301
PariAgrawal1234 <pari.agrawal1314@gmail.com>
2026-05-20 17:10:30 +0530

    2 Async Projects

--- Javascript/Projects/05_Unlimited_Colors/Script.js
+++ Javascript/Projects/05_Unlimited_Colors/Script.js
@@ -1,14 +1,20 @@
-const print = function (str) {
-    console.log(str, Date.now());
+const changeColor = function(){
+    const hex = "0123456789ABCDEF";
+    let color = '#';
+    for(let i = 0; i < 6; i++){
+        color += hex[Math.floor(Math.random()*16)];
+    }
+    document.querySelector('body').style.backgroundColor = color;
 }
     
 let stopme;
 
 document.getElementById('Start').addEventListener('click', function () {
-    if(!stopme) stopme = setInterval(print, 1000, "hi");
+    if(!stopme) stopme = setInterval(changeColor, 1000);
 });
 
 document.getElementById('Stop').addEventListener('click', function () {
     clearTimeout(stopme);
     console.log("STOPPED!");
+    stopme = '';
 })
